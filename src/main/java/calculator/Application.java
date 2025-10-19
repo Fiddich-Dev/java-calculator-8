ea39fbddfee8f2bd0212152237cb5fe4f39badb4
Fiddich <hiws99@naver.com>
2025-10-19 16:20:21 +0900

    feat(input, output): 입력값 입력, 결과값 반환 기능 추가
    주어진 라이브러리를 사용해 입력값 받음
    주어진 템플릿에 맞게 반환
    
    Closes #2

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 입력값 받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        // 반환값
+        int output = 0;
+        System.out.println("결과 : " + output);
     }
 }
